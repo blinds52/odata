6bdb966925f1ec4f921cda76f2cd1204c3d927c1
ralfhandl <ralfhandl@bd6b2f89-7254-4499-94be-07e661c19ab3>
2014-11-18 15:55:55 +0000

    Transformation: Edm.Stream, DefaultValue, miscellaneous.xml: all examples from Part 3: CSDL Validation of primitive type examples from JSON Format chapter 7

---
+++ spec/examples/validate.cmd
@@ -0,0 +1,12 @@
+@echo off
+set FILES=csdl-16.1.json csdl-16.2.json ExampleService.json miscellaneous.json MetadataService.json TM1$metadata.json merged_metadata.json
+
+@rem check syntax of all schemas
+java -jar c:\Java\json-schema-validator-2.2.5-lib.jar --syntax edm.json %FILES%
+@rem validate metadata documents against edm schema
+java -jar c:\Java\json-schema-validator-2.2.5-lib.jar edm.json %FILES%
+
+@rem validate example responses against metadata document as schema
+java -jar c:\Java\json-schema-validator-2.2.5-lib.jar --fakeroot http://odata.org/schemas/v4.0/ csdl-16.1-Category.schema.json csdl-16.1-Category.json
+java -jar c:\Java\json-schema-validator-2.2.5-lib.jar --fakeroot http://odata.org/schemas/v4.0/ csdl-16.1-Product.schema.json csdl-16.1-Product.json
+java -jar c:\Java\json-schema-validator-2.2.5-lib.jar --fakeroot http://odata.org/schemas/v4.0/ miscellaneous-PrimitiveTypes.schema.json miscellaneous-PrimitiveTypes.json
