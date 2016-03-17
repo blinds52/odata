7540f5a04cbac06a47b13bbdb0bb24e9a14c74bc
handl <handl@bd6b2f89-7254-4499-94be-07e661c19ab3>
2016-03-17 12:47:09 +0000

    Replaced Swagger with OpenAPI

--- spec/examples/validate.cmd
+++ spec/examples/validate.cmd
@@ -1,18 +1,18 @@
 @echo off 
-set FILES=csdl-16.1.swagger.json Northwind.swagger.json ExampleService.swagger.json People.swagger.json Products.swagger.json Example.swagger.json TM1.swagger.json TripPin.swagger.json
-set ANNOS=csdl-16.2.swagger.json
+set FILES=csdl-16.1.openapi.json Northwind.openapi.json ExampleService.openapi.json People.openapi.json Products.openapi.json Example.openapi.json TM1.openapi.json TripPin.openapi.json
+set ANNOS=csdl-16.2.openapi.json
 set VOCABS=Core Measures Capabilities Aggregation
 
 @rem check syntax of edm schema
 java -jar c:\Java\json-schema-validator-2.2.5-lib.jar --syntax ../schemas/edm.json
 
-@rem validate metadata documents against swagger schema
-java -jar c:\Java\json-schema-validator-2.2.5-lib.jar swagger.schema.json %FILES%
+@rem validate metadata documents against openapi schema
+java -jar c:\Java\json-schema-validator-2.2.5-lib.jar openapi.schema.json %FILES%
 
 @rem validate annotation files against edm schema
 java -jar c:\Java\json-schema-validator-2.2.5-lib.jar ../schemas/edm.json %ANNOS%
 
 @rem validate vocabularies against edm schema
 for %%V in (%VOCABS%) do (
-  java -jar c:\Java\json-schema-validator-2.2.5-lib.jar ../schemas/edm.json Org.OData.%%V.V1.swagger.json
+  java -jar c:\Java\json-schema-validator-2.2.5-lib.jar ../schemas/edm.json Org.OData.%%V.V1.openapi.json
 )
