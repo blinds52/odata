7f9301634064a81f1342d93326da79178b4b8d63
handl <handl@bd6b2f89-7254-4499-94be-07e661c19ab3>
2016-06-29 16:10:22 +0000

    Minor modification of descriptions

--- trunk/spec/examples/transform.cmd
+++ trunk/spec/examples/transform.cmd
@@ -1,26 +1,40 @@
 @echo off 
-@rem XSLT command-line see https://xml.apache.org/xalan-j/commandline.html
-
-@rem TODO: accept command line parameters, only pull those from transform.txt, all if no parameters are provided
-
 setlocal
 
+@rem XSLT command-line see https://xml.apache.org/xalan-j/commandline.html
+
 set CLASSPATH=%CLASSPATH%;C:\eclipse-Luna\plugins\org.apache.xml.serializer_2.7.1.v201005080400.jar;C:\eclipse-Luna\plugins\org.apache.xalan_2.7.1.v201005080400.jar
+set done=false
 
 for /F "eol=# tokens=1,2,3,4" %%F in (%~n0.txt) do (
-  echo %%~nF 
-  
-  java org.apache.xalan.xslt.Process -XSL V4-CSDL-to-openapi.xsl -PARAM scheme %%G -PARAM host %%H -PARAM basePath %%I -IN %%F -OUT %%~nF.jsontmp
-
-  c:\git\yajl\build\yajl-2.1.1\bin\json_reformat.exe < %%~nF.jsontmp > %%~nF.openapi.json
-  if not errorlevel 1 (
-    del %%~nF.jsontmp
-    if exist %%~nF.openapi-goal.json (
-      c:\bin\diff.exe --ignore-space-change --strip-trailing-cr %%~nF.openapi-goal.json %%~nF.openapi.json
-    ) else (
-      c:\bin\diff.exe --ignore-space-change --strip-trailing-cr ..\vocabularies\%%~nF.json %%~nF.openapi.json
-    )
-  )
+	if [%1] == [%%~nF] (
+	    set done=true
+		call :process %%F %%G %%H %%I
+	) else if [%1]==[] (
+	    set done=true
+		call :process %%F %%G %%H %%I
+	)
 )
 
+if %done%==false echo Don't know how to %~n0 %1
+
 endlocal
+exit /b
+
+
+:process
+
+  echo %~n1
+
+  java org.apache.xalan.xslt.Process -XSL V4-CSDL-to-openapi.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -IN %1 -OUT %~n1.jsontmp
+
+  c:\git\yajl\build\yajl-2.1.1\bin\json_reformat.exe < %~n1.jsontmp > %~n1.openapi.json
+  if not errorlevel 1 (
+    del %~n1.jsontmp
+    if exist %~n1.openapi-goal.json (
+      c:\bin\diff.exe --ignore-space-change --strip-trailing-cr %~n1.openapi-goal.json %~n1.openapi.json
+    ) else (
+      c:\bin\diff.exe --ignore-space-change --strip-trailing-cr ..\vocabularies\%~n1.json %~n1.openapi.json
+    )
+  )
+exit /b
