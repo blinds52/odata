1b57a039c8385e5b4ca234a9db7dd8a567e692be
handl <handl@bd6b2f89-7254-4499-94be-07e661c19ab3>
2016-11-11 13:34:42 +0000

    Example service from Data Aggregation Extension

--- trunk/4.01 spec/examples/transform-openapi.cmd
+++ trunk/4.01 spec/examples/transform-openapi.cmd
@@ -32,10 +32,6 @@ exit /b
   c:\git\yajl\build\yajl-2.1.1\bin\json_reformat.exe < %~n1.jsontmp > %~n1.openapi.json
   if not errorlevel 1 (
     del %~n1.jsontmp
-    if exist %~n1.openapi-goal.json (
-      c:\bin\diff.exe --ignore-space-change --strip-trailing-cr %~n1.openapi-goal.json %~n1.openapi.json
-    ) else (
-      c:\bin\diff.exe --ignore-space-change --strip-trailing-cr ..\vocabularies\%~n1.json %~n1.openapi.json
-    )
+    c:\bin\diff.exe --ignore-space-change --strip-trailing-cr %~n1.openapi-goal.json %~n1.openapi.json
   )
 exit /b
