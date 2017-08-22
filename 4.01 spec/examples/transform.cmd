cf91ffc9cbe3b8b03d0bbe76b2a1b3ae1fbba695
handl <handl@bd6b2f89-7254-4499-94be-07e661c19ab3>
2017-08-22 11:51:27 +0000

    Included schema validation in transform.cmd

--- 4.01 spec/examples/transform.cmd
+++ 4.01 spec/examples/transform.cmd
@@ -1,8 +1,8 @@
 @echo off 
 setlocal
 
-@rem  This script uses Apache Xalan 2.7.1 as XSLT processor
-@rem  For a description of Xalan command-line parameters see http://xalan.apache.org/old/xalan-j/commandline.html
+@rem  This script uses the Apache Xalan 2.7.1 XSLT processor
+@rem  For a description of Xalan command-line parameters see http://xalan.apache.org/xalan-j/commandline.html
 @rem
 @rem  Prerequisites
 @rem  - Java SE 8 is installed and in the PATH - download from http://www.oracle.com/technetwork/java/javase/downloads/index.html 
@@ -15,6 +15,8 @@ set ODATA-VOCABULARIES=c:\git\odata-vocabularies
 set ODATA-OPENAPI=c:\git\odata-openapi
 @rem  - YAJL's json_reformat from https://github.com/lloyd/yajl has been compiled and environment variable YAJL_REFORMAT set to its location
 set YAJL_REFORMAT=c:\git\yajl\build\yajl-2.1.1\bin\json_reformat.exe
+@rem  - Node.js is istalled - download from https://nodejs.org
+@rem  - https://github.com/zaggino/z-schema is installed: npm install z-schema
 
 set done=false
 
@@ -52,5 +54,12 @@ exit /b
     del %~n1.normalized.xml %~n1.tmp.json
     if [%2]==[V2] del %~n1.V4.xml
     diff.exe --ignore-space-change --strip-trailing-cr %~n1-goal.json %~n1.json
+    
+    call z-schema --ignoreUnknownFormats --pedanticCheck ../schemas/csdl.schema.json %~n1.json > %~n1.log
+    if %ERRORLEVEL% == 1 (
+      type %~n1.log
+    ) else (
+      del %~n1.log
+    )
   )
 exit /b
