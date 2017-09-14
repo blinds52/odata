2fcd9fa491f8d55bc2c7125dce8346a609531080
handl <handl@bd6b2f89-7254-4499-94be-07e661c19ab3>
2017-09-14 10:24:25 +0000

    Integrated validation based on @odata.context

--- trunk/4.01 spec/examples/JSONSchema/validate.cmd
+++ trunk/4.01 spec/examples/JSONSchema/validate.cmd
@@ -1,8 +1,10 @@
 @echo off
 
-call ajv validate -s csdl-16.1-Category.jsonschema -d csdl-16.1-Category.json -r csdl-16.1.jsonschema -m edm.json --unknown-formats=int32 --unknown-formats=decimal
-
-call ajv validate -s csdl-16.1-Product.jsonschema -d csdl-16.1-Product.json -r csdl-16.1.jsonschema -m edm.json --unknown-formats=int32 --unknown-formats=decimal
+call ajv validate -s csdl-16.1.jsonschema -d csdl-16.1-Categories.json -m edm.json --unknown-formats=int32 --unknown-formats=decimal
+call ajv validate -s csdl-16.1.jsonschema -d csdl-16.1-Category.json -m edm.json --unknown-formats=int32 --unknown-formats=decimal
+call ajv validate -s csdl-16.1.jsonschema -d csdl-16.1-Category-projected.json -m edm.json --unknown-formats=int32 --unknown-formats=decimal
+call ajv validate -s csdl-16.1.jsonschema -d csdl-16.1-MainSupplier.json -m edm.json --unknown-formats=int32 --unknown-formats=decimal
+call ajv validate -s csdl-16.1.jsonschema -d csdl-16.1-Product.json -m edm.json --unknown-formats=int32 --unknown-formats=decimal
 
 call ajv validate -s miscellaneous-PrimitiveTypes.jsonschema -d miscellaneous-PrimitiveTypes.json -r miscellaneous.jsonschema -m edm.json --unknown-formats=int32 --unknown-formats=decimal --unknown-formats=base64url --unknown-formats=double --unknown-formats=single --unknown-formats=duration --unknown-formats=int64
 
