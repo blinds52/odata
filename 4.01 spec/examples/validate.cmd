b4dc8c30dc133c67df92d6f2a90f1be329fa33e8
handl <handl@bd6b2f89-7254-4499-94be-07e661c19ab3>
2017-11-28 09:51:25 +0000

    transform.cmd now uses ajv-cli miscellaneous.xml: added synonym member to enumeration type

--- 4.01 spec/examples/validate.cmd
+++ 4.01 spec/examples/validate.cmd
@@ -1,22 +1,31 @@
 @echo off 
 setlocal
 
-rem JSON Schema Validators used
-rem - https://github.com/zaggino/z-schema -> npm install z-schema
-rem - https://github.com/daveclayton/json-schema-validator -> download jar from https://bintray.com/fge/maven/json-schema-validator/view
+@rem  Prerequisites
+@rem  - Node.js is installed - download from https://nodejs.org/
+@rem  - ajv-cli is installed - npm install -g ajv-cli
 
-rem echo csdl.schema.json
-rem java -jar c:\Java\json-schema-validator-2.2.6-lib.jar --syntax ../schemas/csdl.schema.json %FILES%
+set done=false
+set here=%~dp0
 
-
-for /F "eol=# tokens=1" %%F in (transform.txt) do (
-	if /I [%~n1] == [%%~nF] (
-	  set done=true
-		call :process %%F
-	) else if [%1]==[] (
-	  set done=true
-		call :process %%F
-	)
+if [%1]==[] (
+  set done=true
+  for /F "eol=# tokens=1,2" %%F in (transform.txt) do (
+		call :process %%F %%G
+  )
+) else if exist %1\ (
+  set done=true
+  cd %1
+  for %%F in (*.xml) do (
+    call :process %%F
+  )
+) else (
+  for /F "eol=# tokens=1,2" %%F in (transform.txt) do (
+    if /I [%~n1]==[%%~nF] (
+      set done=true
+      call :process %%F %%G
+    )
+  )
 )
 
 if %done%==false echo Don't know how to %~n0 %1
@@ -28,13 +37,6 @@ exit /b
 :process
   echo %~n1
 
-  rem java -jar c:\Java\json-schema-validator-2.2.6-lib.jar ../schemas/csdl.schema.json %~n1.json
-
-  call z-schema --ignoreUnknownFormats --pedanticCheck ../schemas/csdl.schema.json %~n1.json > %~n1.log
-  if %ERRORLEVEL% == 1 (
-    type %~n1.log
-  ) else (
-    del %~n1.log
-  )
+  call ajv -s %here%\..\schemas\csdl.schema.json -d %~n1.json > nul
 
 exit /b
